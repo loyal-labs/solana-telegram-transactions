8415cb1735f081dc4d1a404f703362f3f412f51f
Chris Cherniakov <taequn@gmail.com>
2025-10-18 17:38:01 -0700

    feat(lib/telegram): add telegram helpers

---
+++ frontend/src/lib/telegram/index.ts
@@ -0,0 +1,61 @@
+import { isTMA } from "@telegram-apps/bridge";
+import {
+  init,
+  miniApp,
+  sendData,
+  requestContact,
+  RequestedContact,
+  openTelegramLink,
+} from "@telegram-apps/sdk-react";
+
+// Initialize once
+let initialized = false;
+
+export const initTelegram = () => {
+  if (!initialized && isTMA()) {
+    try {
+      init();
+      miniApp.ready();
+      initialized = true;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+};
+
+// Core functions
+export const isInMiniApp = () => {
+  if (typeof window === "undefined") return false;
+
+  // Initialize if not already done
+  initTelegram();
+
+  // Check if miniApp is available after init
+  return miniApp.isSupported();
+};
+
+export const requestTelegramContact = async (): Promise<RequestedContact> => {
+  if (!requestContact.isAvailable()) throw new Error("Not available");
+
+  return await requestContact();
+};
+
+export const openTgLink = (link: string) => {
+  if (!openTelegramLink.isAvailable()) {
+    throw new Error("Not available");
+  }
+
+  openTelegramLink(link);
+};
+
+export const sendString = (data: string) => {
+  if (sendData.isAvailable()) {
+    sendData(data);
+
+    return true;
+  }
+
+  return false;
+};
+
+export type { RequestedContact };
