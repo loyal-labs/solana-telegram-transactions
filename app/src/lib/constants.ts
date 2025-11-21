0e7aa86691a8e477c80a2a9a9b4bcdaf63e05001
Chris Cherniakov <taequn@gmail.com>
2025-11-20 18:25:42 -0800

    feat(solana/verification): create function to verify initData onchain

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -3,11 +3,21 @@ export const SECRET_KEY_STORAGE_KEY = "solana_secret_key";
 
 export const TELEGRAM_BOT_ID = "8065140499";
 
+export const TELEGRAM_PUBLIC_KEY_PROD =
+  "e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d";
+export const TELEGRAM_PUBLIC_KEY_PROD_BYTES = Buffer.from(
+  TELEGRAM_PUBLIC_KEY_PROD,
+  "hex"
+);
+export const TELEGRAM_PUBLIC_KEY_PROD_UINT8ARRAY = new Uint8Array(
+  TELEGRAM_PUBLIC_KEY_PROD_BYTES
+);
+
 export const TELEGRAM_PUBLIC_KEYS = [
   // Test environment key
   "40055058a4ee38156a06562e52eece92a771bcd8346a8c4615cb7376eddf72ec",
   // Production key
-  "e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d",
+  TELEGRAM_PUBLIC_KEY_PROD,
 ];
 
 export const DEPOSIT_SEED = "deposit";
