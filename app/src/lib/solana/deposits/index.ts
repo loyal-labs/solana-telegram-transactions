fb7b9a9d1f5e251380515767bffe17482fac3b1a
Chris Cherniakov <taequn@gmail.com>
2025-11-21 04:56:46 -0800

    feat(solana): create fetch-deposits function to abstract complexity away

---
+++ app/src/lib/solana/deposits/index.ts
@@ -0,0 +1,13 @@
+import { claimDeposit } from "./claim-deposit";
+import { getDeposit } from "./get-deposit";
+import { getDepositWithUsername } from "./get-deposit-with-username";
+import { refundDeposit } from "./refund-deposit";
+import { topUpDeposit } from "./top-up-deposit";
+
+export {
+  claimDeposit,
+  getDeposit,
+  getDepositWithUsername,
+  refundDeposit,
+  topUpDeposit,
+};
---
+++ app/src/lib/solana/verification/index.ts
@@ -0,0 +1,11 @@
+import { fetchSessionData } from "./fetch-session-data";
+import { storeInitData } from "./store-init-data";
+import type { TelegramSessionData } from "./types";
+import { verifyInitData } from "./verify-init-data";
+
+export {
+  fetchSessionData,
+  storeInitData,
+  type TelegramSessionData,
+  verifyInitData,
+};
