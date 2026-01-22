659a9f172a1818d198bcbbfe2cf1018cde0c97e8
Chris Cherniakov <taequn@gmail.com>
2026-01-21 20:24:11 -0800

    fix(telegram/bot-api): update custom emoji ID

--- app/src/lib/telegram/bot-api/constants.ts
+++ app/src/lib/telegram/bot-api/constants.ts
@@ -1,3 +1,3 @@
 export const CHANNEL_USERNAME = "@loyal_tg";
-export const CUSTOM_EMOJI_ID = "5262532726669476202";
+export const CUSTOM_EMOJI_ID = "5361803602862052361";
 export const MINI_APP_LINK = "https://t.me/askloyal_tgbot/app";
