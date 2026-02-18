fa348c5a284498255a00645b74a099ee21d739b8
Sviatoslav Alekseev <sviatoslav.alekseev@proton.me>
2026-02-18 14:03:49 +0100

    fix(app/solana): use solana devnet wss rpc
    Failed to connect to Helius WSS

--- app/src/lib/solana/rpc/constants.ts
+++ app/src/lib/solana/rpc/constants.ts
@@ -3,10 +3,14 @@ export const SECURE_MAINNET_RPC_URL =
 export const SECURE_MAINNET_RPC_WS =
   "wss://guendolen-nvqjc4-fast-mainnet.helius-rpc.com";
 
-export const SECURE_DEVNET_RPC_URL =
-  "https://aurora-o23cd4-fast-devnet.helius-rpc.com";
-export const SECURE_DEVNET_RPC_WS =
-  "wss://aurora-o23cd4-fast-devnet.helius-rpc.com";
+// FIXME: Failed to connect to Helius WSS
+// export const SECURE_DEVNET_RPC_URL =
+//   "https://aurora-o23cd4-fast-devnet.helius-rpc.com";
+// export const SECURE_DEVNET_RPC_WS =
+//   "wss://aurora-o23cd4-fast-devnet.helius-rpc.com";
+
+export const SECURE_DEVNET_RPC_URL = "https://api.devnet.solana.com";
+export const SECURE_DEVNET_RPC_WS = "wss://api.devnet.solana.com";
 
 export const TESTNET_RPC_URL = "https://api.testnet.solana.com";
 export const TESTNET_RPC_WS = "wss://api.testnet.solana.com";
