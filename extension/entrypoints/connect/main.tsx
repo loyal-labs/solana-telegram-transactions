86e93842559647aa99cc86d3343a6ac48d81dd49
Vlad Arbatov <v.o.arbatov@gmail.com>
2026-03-18 18:08:32 +0300

    fix(extension): add Buffer polyfill for Solana libraries
    Solana web3.js requires Buffer which is not available in browser
    extension context. Add buffer package and import polyfill as first
    statement in all entrypoints.

--- extension/entrypoints/connect/main.tsx
+++ extension/entrypoints/connect/main.tsx
@@ -1,3 +1,4 @@
+import "~/src/lib/polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
--- extension/entrypoints/sidepanel/main.tsx
+++ extension/entrypoints/sidepanel/main.tsx
@@ -1,3 +1,4 @@
+import "~/src/lib/polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "~/assets/tailwind.css";
