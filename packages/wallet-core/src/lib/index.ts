0b58c4d492185aa807d5d0c26ddb3896c8b68b85
Vlad Arbatov <v.o.arbatov@gmail.com>
2026-03-18 14:21:41 +0300

    feat(wallet-core): extract Jupiter swap client

--- packages/wallet-core/src/lib/index.ts
+++ packages/wallet-core/src/lib/index.ts
@@ -1,3 +1,5 @@
+export * from "./jupiter/client";
+export * from "./jupiter/types";
 export { getTokenIconUrl } from "./token-icon";
 export * from "./solana/wsol-adapter";
 export * from "./solana/sign-in";
